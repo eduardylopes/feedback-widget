1263b3f793576d54efef740086887a8eaf547408
Eduardy Lopes de Morais <eduardylopes@gmail.com>
2022-05-05 20:40:11 -0300

    fix import images bug

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,8 +1,8 @@
 import CloseButton from '../CloseButton';
 
-import bugImageUrl from '../assets/bug.svg';
-import ideaImageUrl from '../assets/idea.svg';
-import thoughtImageUrl from '../assets/thought.svg';
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
 import React, { useState } from 'react';
 import FeedbackTypeStep from './Steps/FeedbackTypeStep';
 
